90525b2cf1fc15d0d9d9c4c31219771aab1814ef
Cameron Gerson <2027gerson.cameron@fcboe.org>
2024-01-24 13:37:06 -0500

    Did the thing

---
+++ basics.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("This is the CBasics file\n");
+
+	int num;
+	printf("Enter a number: ");
+	scanf("%i", &num);
+	printf("You entered number %i\n", num);
+	return 0;
+}
